ea988b89578f99e48df0fa7f66e4130254d0d837
paul yoel antezana <paul.antezana.2@gmail.com>
2020-02-04 14:41:35 -0500

    new updates

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    
     require_once __DIR__ . '/src/autoload.php';
 
     session_start();
 
-    // $router = new Router();
-    // $router->run();
+    $router = new Router();
+    $router->run();
